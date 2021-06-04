053d25d09d35cdde91950a3ccbe7aca5e7f81c02
moi061805 <84187816+moi061805@users.noreply.github.com>
2021-06-04 07:47:09 -0400

    Update Player.java

--- src/game/Player.java
+++ src/game/Player.java
@@ -20,34 +20,33 @@ public class Player {
     public int getHighScore() {return highScore;}
     public int gethealth() {return health;}
     public int getBar() { return bar;}
-
+//it increments the round score
     public void increment() { roundScore++; }
-
+//it increments the highscore
     public void incrementHighScore(){ ++highScore; }
-    
+//it increments the health
     public void incrementHealth() { ++health;}
-    
+//it decreasses the health
     public void decreassHealth() { --health; }
-    
+//it makes a critical taking out 2 health
     public void critical() { health -= 2;}
-    
+//it increments the critical bar
     public void incrementBar() { bar++; }
-    
+//it makes a super critical taking out 3 health
     public void superCritical() { health -= 3; }
-    
-    //public int setHighScore() { return ++highScore; }
-
+//it resets the round score
     public void resetRoundScore(){ roundScore = 0; }
-    
+//it resets the bar
     public void resetBar() { bar = 0; }
-
+//it resets the highscore
     public void resetHighScore(){
         highScore = 0;
     }
 
     Random random = new Random();
+    //it inputs the guess of the player
     public void inputGuess(){
-        boolean valid = false; //Variable that becomes true if the string input is correct
+        boolean valid = false; 
         while(!valid) {
             guess = JOP.in("Pick rock, paper or scissors.");
             if (!guess.equalsIgnoreCase("rock") && !guess.equalsIgnoreCase("paper") && !guess.equalsIgnoreCase("scissors")) {
@@ -57,6 +56,7 @@ public class Player {
             }
         }
     }
+    //it inputs the guess of the computer
     public void computerGuess(){
         //0 is rock, 1 is paper, 2 is scissors
         int computerNum = random.nextInt(2);
